9025049bf5c246243897b117f3afb545d043bb2b
LeonardoBonaiuti <leonardo.bonaiuti@studenti.gobettivolta.edu.it>
2024-06-06 00:24:57 +0200

    gnmh

--- app/src/main/java/com/example/heartraining_androidapp/ChordRecog_play.java
+++ app/src/main/java/com/example/heartraining_androidapp/ChordRecog_play.java
@@ -2,13 +2,36 @@ package com.example.heartraining_androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChordRecog_play extends AppCompatActivity {
+
+    Button reapeatChord;
+
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chord_recog_play);
+
+        reapeatChord = findViewById(R.id.button_repeatChord);
+
+        reapeatChord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer == null){
+                    mediaPlayer = MediaPlayer.create(ChordRecog_play.this, R.raw.Aaug);
+                }
+                mediaPlayer.start();
+            }
+        });
+
+
     }
+
+
 }
--- app/src/main/java/com/example/heartraining_androidapp/ChordRecog_settings.java
+++ app/src/main/java/com/example/heartraining_androidapp/ChordRecog_settings.java
@@ -2,13 +2,29 @@ package com.example.heartraining_androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChordRecog_settings extends AppCompatActivity {
+
+    Button playChordRecog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chord_recog_settings);
+
+        playChordRecog = findViewById(R.id.button_play);
+
+        playChordRecog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChordRecog_settings.this, ChordRecog_play.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
